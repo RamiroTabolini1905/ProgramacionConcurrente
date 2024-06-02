704adf1cd19f4f60ad1e0c876058c0445e082044
Ramiro Tabolini <ramirotabolini@gmail.com>
2024-06-01 23:22:14 -0300

    metodos para mostrar las plazas, para verificas si se puede disparar y otro metodo para disparar

--- src/main/java/src/RedDePetri.java
+++ src/main/java/src/RedDePetri.java
@@ -2,7 +2,55 @@ package src;
 
 public class RedDePetri {
     private	int marcadoDeRed[][];
+    private int[][] preIncidencia;
+    private int[][] postIncidencia;
+    private int[][] incidencia;
+    private int[] plazas; // Estado actual de las plazas
 
-    public RedDePetri(){}
+    public RedDePetri(int[][] preIncidencia, int[][] postIncidencia, int[][] incidencia, int[] plazasIniciales) {
+        this.preIncidencia = preIncidencia;
+        this.postIncidencia = postIncidencia;
+        this.incidencia = incidencia;
+        this.plazas = plazasIniciales;
+    }
+
+    public void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printPlazas() {
+        System.out.print("Estado de las plazas: ");
+        for (int i = 0; i < plazas.length; i++) {
+            System.out.print(plazas[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // Verificar si una transición está habilitada
+    public boolean puedeDispararse(int transicion) {
+        for (int i = 0; i < plazas.length; i++) {
+            if (plazas[i] < preIncidencia[i][transicion]) {
+                return false; // La transición no puede dispararse
+            }
+        }
+        return true;
+    }
+
+    // Disparar una transición
+    public void dispararTransicion(int transicion) {
+        if (puedeDispararse(transicion)) {
+            for (int i = 0; i < plazas.length; i++) {
+                plazas[i] = plazas[i] - preIncidencia[i][transicion] + postIncidencia[i][transicion];
+            }
+            System.out.println("Transición " + transicion + " disparada.");
+        } else {
+            System.out.println("Transición " + transicion + " no puede dispararse.");
+        }
+    }
 
 }
